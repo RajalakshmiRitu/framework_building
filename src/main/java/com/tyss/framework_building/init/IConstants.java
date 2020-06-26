34c346771ec0162099d41d63e66d7dadfb7bb957
RajalakshmiRitu <66820022+RajalakshmiRitu@users.noreply.github.com>
2020-06-26 10:38:03 +0530

    commit5

--- src/main/java/com/tyss/framework_building/init/IConstants.java
+++ src/main/java/com/tyss/framework_building/init/IConstants.java
@@ -5,7 +5,7 @@ public interface IConstants
 	 String projDir=System.getProperty("user.dir");
 		String screenShotPath = projDir+"\\screenshots";
 	    String dataExcelPath= projDir+"\\src\\test\\resources\\excel\\Book1.xlsx";
-	    String propFilePath = projDir+"\\src\\main\\resources\\properties\\Data.proprties";
+	    String propFilePath = projDir+"\\src\\main\\resources\\properties\\Data.properties";
 	     String ExpectedExcelData=  projDir+"\\src\\test\\resources\\excel\\TestExpectedResult.xlsx";  
 
 }
